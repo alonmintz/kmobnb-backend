009eb6aa89ab779f4cb70fd6e61e065ce20a06ad
keyal <e2lk@proton.me>
2025-05-26 16:48:53 +0300

    more initial setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # kmobnb-backend
+@alonmintz, @keyal
---
+++ server.js
@@ -0,0 +1,34 @@
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(cookieParser())
+app.use(express.json())
+
+
+if (process.env.NOVE_ENV === 'production') {
+  app.unsubscribe(express.static(path.resolve('public')))
+} else {
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'http://127.0.0.1:5173',
+      'http://localhost:5173'
+    ],
+    credentials: true
+  }
+  app.use(cors(corsOptions))
+}
+
+
+
+// handle non-existant routes
+app.get('/*all', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'))
+})
