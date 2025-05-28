e05f7683a93ef351658b0145448924aa0c37ecda
keyal <e2lk@proton.me>
2025-05-28 21:12:34 +0300

    reorged npm run scripts

--- config/index.js
+++ config/index.js
@@ -1,15 +1,28 @@
-import configProd from './prod.js'
-import configDev from './dev.js'
+// import configProd from './prod.js'
+// import configLocal from './dev.js'
 
 export var config
 if (process.env.NODE_ENV === 'production') {
-    config = configProd
-} else {
-    config = configDev
+    config = {
+        dbURL: process.env.MONGO_URL,
+        dbName: process.env.DB_NAME
+    }
+    console.log('config:', config)
+}
+else if (process.env.NODE_ENV === 'remoteDev') {
+    config = {
+        dbURL: process.env.REMOTE_DEV_MONGO_URL,
+        dbName: process.env.REMOTE_DEV_DB_URL
+    }
+    console.log('config:', config)
+}
+else {
+    config = {
+        dbURL: 'mongodb://127.0.0.1:27017',
+        dbName: 'kmobnb'
+    }
+    console.log('config:', config)
 }
 
 //* Uncomment the following line to use guest mode
 // config.isGuestMode = true
-
-//* Uncomment the following line to use the production configuration (Mongo Atlas DB)
-// config = configProd
