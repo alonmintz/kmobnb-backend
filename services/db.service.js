0666aae902cec0ea441784c80317daa53c1f4271
alonmintz <92505398+alonmintz@users.noreply.github.com>
2025-06-06 00:09:26 +0300

    fixed criteria not filtering status: added conditional status filtering depending on list type. fix stay status update killing host data

--- services/db.service.js
+++ services/db.service.js
@@ -1,30 +1,30 @@
-import { MongoClient } from 'mongodb'
-import { config } from '../config.js'
-import { logger } from './logger.service.js'
+import { MongoClient } from "mongodb";
+import { config } from "../config.js";
+import { logger } from "./logger.service.js";
 
-export const dbService = { getCollection }
+export const dbService = { getCollection };
 
-var dbConn = null
+var dbConn = null;
 
 async function getCollection(collectionName) {
-    try {
-        const db = await _connect()
-        const collection = await db.collection(collectionName)
-        return collection
-    } catch (err) {
-        logger.error('Failed to get Mongo collection', err)
-        throw err
-    }
+  try {
+    const db = await _connect();
+    const collection = await db.collection(collectionName);
+    return collection;
+  } catch (err) {
+    logger.error("Failed to get Mongo collection", err);
+    throw err;
+  }
 }
 
 async function _connect() {
-    if (dbConn) return dbConn
+  if (dbConn) return dbConn;
 
-    try {
-        const client = await MongoClient.connect(config.dbURL)
-        return dbConn = client.db(config.dbName)
-    } catch (err) {
-        logger.error('Cannot Connect to DB', err)
-        throw err
-    }
+  try {
+    const client = await MongoClient.connect(config.dbURL);
+    return (dbConn = client.db(config.dbName));
+  } catch (err) {
+    logger.error("Cannot Connect to DB", err);
+    throw err;
+  }
 }
